8286963d47752f048891c5f5e10d2c267311b07d
NakshGargDev <nakshgarg677@gmail.com>
2026-08-10 17:53:59 +0530

    Improve UI

--- src/inputs/movePiece.js
+++ src/inputs/movePiece.js
@@ -1,6 +1,7 @@
 import { game } from "../game";
 import { pieces } from "../pieces";
 import { isCheckmate } from "../rules/checkmate.js";
+import { findKing } from "../rules/findKing.js";
 import { isInCheck } from "../rules/isInCheck.js"
 
 export function movePiece(board, square, row, col) {
@@ -18,20 +19,29 @@ export function movePiece(board, square, row, col) {
 	game.selected = null;
 	game.selectedSquare = null;
 
+	document.querySelectorAll(".in-check")
+		.forEach(sq => sq.classList.remove("in-check"));
+
 	game.turn = game.turn === "white" ? "black" : "white";
 
-	const check = isInCheck(board, game.turn === "white")
+	const message = document.querySelector("#game-message");
 
-	if (check) {
-		console.log("check");
+	if (isCheckmate(board, game.turn === "white")) {
+		message.textContent = "CHECKMATE!";
+		console.log(isCheckmate(board, game.turn === "white"));
+	} else if (isInCheck(board, game.turn === "white")) {
+		const kingPos = findKing(board, game.turn === "white");
 
-		const checkmate = isCheckmate(board, game.turn === "white");
+		const kingSquare = document.querySelector(
+			`.square[data-row="${kingPos[0]}"][data-col="${kingPos[1]}"]`
+		);
 
-		if (checkmate) {
-			console.log("checkmate");
-		}
+		kingSquare.classList.add("in-check");
+
+		message.textContent = "CHECK!";
+	} else {
+		message.textContent = "";
 	}
-
 	document.querySelectorAll(".selected")
 		.forEach(sq => sq.classList.remove("selected"));
 }
--- src/rules/checkmate.js
+++ src/rules/checkmate.js
@@ -5,24 +5,23 @@ export function isCheckmate(board, isWhite) {
 
 	const check = isInCheck(board, isWhite);
 
-	if (check) {
+	if (!check) return;
 
-		for (let row = 0; row < 8; row++) {
-			for (let col = 0; col < 8; col++) {
+	for (let row = 0; row < 8; row++) {
+		for (let col = 0; col < 8; col++) {
 
-				const piece = board[row][col];
+			const piece = board[row][col];
 
-				if (piece === "") continue;
+			if (piece === "") continue;
 
-				const player = isWhite ? piece === piece.toUpperCase() : piece === piece.toLowerCase()
+			const player = isWhite ? piece === piece.toUpperCase() : piece === piece.toLowerCase()
 
-				if (player) {
+			if (player) {
 
-					const moves = getLegalMoves(board, row, col);
+				const moves = getLegalMoves(board, row, col);
 
-					if (moves.length > 0) return false;
+				if (moves.length > 0) return false;
 
-				}
 			}
 		}
 	}
